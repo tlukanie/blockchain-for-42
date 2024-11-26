a0aa43ddd5cef22e5587104c2b182258c453f05b
tlukanie <tanyanewem@gmail.com>
2024-11-26 14:12:30 +0200

    working on events

--- contracts/HelloBlockchain.sol
+++ contracts/HelloBlockchain.sol
@@ -1,15 +1,14 @@
 pragma solidity >=0.4.25 <0.9.0;
 
 contract HelloBlockchain {
-    //enum StateType { Request, Respond }
-	enum StateType {Request}
+    enum StateType { Request }
 
-    StateType public  State;
-    address public  Requestor;
-    //address public  Responder;
-
+    StateType public State;
+    address public Requestor;
     string public RequestMessage;
-    //string public ResponseMessage;
+
+    // Declare the event
+    event RequestSent(address indexed requestor, string requestMessage, StateType state);
 
     constructor(string memory message) {
         Requestor = msg.sender;
@@ -17,16 +16,49 @@ contract HelloBlockchain {
         State = StateType.Request;
     }
 
-    // call this function to send a request
+    // Call this function to send a request
     function SendRequest(string memory requestMessage) public {
-
         if (Requestor != msg.sender) {
             revert();
         }
 
         RequestMessage = requestMessage;
         State = StateType.Request;
+
+        // Emit the event
+        emit RequestSent(msg.sender, requestMessage, State);
     }
+}
+
+// pragma solidity >=0.4.25 <0.9.0;
+
+// contract HelloBlockchain {
+//     //enum StateType { Request, Respond }
+// 	enum StateType {Request}
+
+//     StateType public  State;
+//     address public  Requestor;
+//     //address public  Responder;
+
+//     string public RequestMessage;
+//     //string public ResponseMessage;
+
+//     constructor(string memory message) {
+//         Requestor = msg.sender;
+//         RequestMessage = message;
+//         State = StateType.Request;
+//     }
+
+//     // call this function to send a request
+//     function SendRequest(string memory requestMessage) public {
+
+//         if (Requestor != msg.sender) {
+//             revert();
+//         }
+
+//         RequestMessage = requestMessage;
+//         State = StateType.Request;
+//     }
 
     // // call this function to send a response
     // function SendResponse(string memory responseMessage) public {
@@ -35,4 +67,4 @@ contract HelloBlockchain {
     //     ResponseMessage = responseMessage;
     //     State = StateType.Respond;
     // }
-}
+//}
