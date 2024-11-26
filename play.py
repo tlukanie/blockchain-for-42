4506cb9ee4263d57dfdb772d78860902140627eb
tlukanie <tanyanewem@gmail.com>
2024-11-26 15:37:44 +0200

    started play.py

---
+++ play.py
@@ -0,0 +1,16 @@
+import json
+from solcx import compile_standard
+from web3 import Web3
+
+player_1 = int(input("Enter number for the Player #1: "))
+player_2 = int(input("Enter number for the Player #2: "))
+if player_1 > player_2:
+	winner = player_1
+elif player_2 > player_1:
+	winner = player_2
+else:
+	winner = 0
+
+print(winner)
+
+#Extract bytecode and ABI from WinnerStorage json
